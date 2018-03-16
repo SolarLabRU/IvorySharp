d10353b5f99a6221aa044976c9ab260317edfb95
rex-core <NVT{Yj,LEYw[6jsWUe>
2018-03-17 00:36:08 +0300

    Удалены явные ссылки на пакет NetCore

--- src/IvorySharp/Aspects/MethodInterceptionAspect.cs
+++ src/IvorySharp/Aspects/MethodInterceptionAspect.cs
@@ -10,23 +10,4 @@ namespace IvorySharp.Aspects
         /// <inheritdoc />
         public abstract void OnInvoke(IInvocation invocation);
     }
-
-    /// <summary>
-    /// Реализация аспекта для перехвата метода, которая ничего не делает (просто вызывает метод).
-    /// </summary>
-    internal class NullMethodInterceptionAspect : MethodInterceptionAspect
-    {
-        /// <summary>
-        /// Экземпляр аспекта.
-        /// </summary>
-        public static NullMethodInterceptionAspect Instance { get; } = new NullMethodInterceptionAspect();
-        
-        private NullMethodInterceptionAspect() { }
-        
-        /// <inheritdoc />
-        public override void OnInvoke(IInvocation invocation)
-        {
-            invocation.Proceed();
-        }
-    }
 }
---
+++ src/IvorySharp/Aspects/NullMethodInterceptionAspect.cs
@@ -0,0 +1,23 @@
+﻿using IvorySharp.Core;
+
+namespace IvorySharp.Aspects
+{
+    /// <summary>
+    /// Реализация аспекта для перехвата метода, которая ничего не делает (просто вызывает метод).
+    /// </summary>
+    internal class NullMethodInterceptionAspect : MethodInterceptionAspect
+    {
+        /// <summary>
+        /// Экземпляр аспекта.
+        /// </summary>
+        public static NullMethodInterceptionAspect Instance { get; } = new NullMethodInterceptionAspect();
+        
+        private NullMethodInterceptionAspect() { }
+        
+        /// <inheritdoc />
+        public override void OnInvoke(IInvocation invocation)
+        {
+            invocation.Proceed();
+        }
+    }
+}
